1d8d96a8c6c39afada7b38868337103485635668
David Gaag <davidgaag3@gmail.com>
2023-03-13 00:15:42 -0600

    Add app.js, song submission form in index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+class Queue {
+    constructor() {
+        this.queueListEl = document.getElementById("queue-list");
+    }
+
+    addSong() {
+        
+        
+    }
+
+}
+
+queue = new Queue();
+
+clearTextFields() {
+    
+}
