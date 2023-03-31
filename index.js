fd1674adccfe48b2b21a73f6fa7f87d420ca360a
David Gaag <davidgaag3@gmail.com>
2023-03-31 02:17:49 -0600

    Edit index.js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
