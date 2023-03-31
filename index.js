b29df0e5cb29b543038a6c857cd226412624d36b
David Gaag <davidgaag3@gmail.com>
2023-03-31 02:13:21 -0600

    Remove deployFiles.sh, add index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
