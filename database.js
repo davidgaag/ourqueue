9fc6029f6cde1d44edc240a31dcd90de3aed4555
David Gaag <davidgaag3@gmail.com>
2023-04-09 11:51:46 -0600

    Add bcrypt, uuid, cookieparser dependencies

--- database.js
+++ database.js
@@ -1,4 +1,6 @@
-const {MongoClient} = require ("mongodb")
+const { MongoClient } = require("mongodb");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 
 const username = process.env.MONGOUSER_STARTUP;
 const password = process.env.MONGOPASSWORD_STARTUP;
@@ -14,4 +16,15 @@ const url = `mongodb+srv://${username}:${password}@${hostName}`;
 const client = new MongoClient(url);
 const userCollection = client.db("ourqueue").collection("user");
 const queueCollection = client.db("ourqueue").collection("queue");
-// TODO
+
+function getUser(username) {
+    return userCollection.findOne({ username: username });
+}
+
+async function registerUser(user) {
+    // TODO
+}
+
+function addSong(song) {
+    // TODO
+}
