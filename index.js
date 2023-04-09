47d1e128b30b2da23e5ec2942a4c4d4eb97e295e
David Gaag <davidgaag3@gmail.com>
2023-04-09 12:42:15 -0600

    Add /auth/login endpoint

--- index.js
+++ index.js
@@ -16,5 +16,46 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+   console.log(`Listening on port ${port}`);
+});
+
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// Create authtoken for new user
+apiRouter.post("/auth/create", async (req, res) => {
+   if (await db.getUser(req.body.username)) {
+      res.status(409).send({ msg: "Existing user" });
+   } else {
+      const user = await db.registerUser(req.body.username, req.body.password);
+
+      setAuthCookie(res, user.token);
+
+      // Sends the HTTP response with the body containing the user ID
+      res.send({
+         id: user._id,
+      });
+   }
+});
+
+// Get authToken for the provided login credentials
+apiRouter.post("/auth/login", async (req, res) => {
+   const user = await db.getUser(req.body.username);
+   if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+         setAuthCookie(res, user.token);
+         res.send({ id: user._id });
+         return;
+      }
+   }
+   res.status(401).send({ msg: "Unauthorized" });
+});
+
+// Sets the cookie in the HTTP response
+function setAuthCookie(res, authToken) {
+   res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: "strict:",
+   });
+}
